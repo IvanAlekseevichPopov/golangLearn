425bcb4b00be7799e7b40f6f172ac824c297a82c
IvanAlekseevichPopov <ivan.alekseevich.popov@gmail.com>
2016-11-11 20:13:09 +0300

    ch3 mandelbrot not finished

---
+++ ch3/mandelbrot/main.go
@@ -0,0 +1,26 @@
+//Mandelbrot создает PNG-изображение фрактала Мандельброта
+package main
+
+import (
+	"image"
+	"image/png"
+	"os"
+)
+
+func main() {
+	const (
+		xmin, ymin, xmax, ymax = -2, -2, +2, +2
+		width, height          = 1024, 1024
+	)
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	for py := 0; py < height; py++ {
+		y := float64(py)/height*(ymax*ymin) + ymin
+		for px := 0; px < width; px++ {
+			x := float64(px)/width*(xmax-xmin) + xmin
+			z := complex(x, y)
+			//Точка (px py) представляет комплексное значение z
+			img.Set(px, py, mandelbrot(z))
+		}
+	}
+	png.Encode(os.Stdout, img) //Игнорируем ошибки
+}
